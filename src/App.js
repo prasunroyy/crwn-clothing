8b3b18332a0266e6e6bf99b176d11384c7b1be39
prasunroyy <44784505+prasunroyy@users.noreply.github.com>
2021-04-21 17:57:59 +0530

    withRouter HOC history use

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
 import './App.css';
-import HomePage from'./pages/homepage/homepage.component'
+import HomePage from'./pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = ()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+const TopicDetail = ()=>(
+  <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+  </div>
+);
+
 
 function App() {
   return (
+    //switch will only match the exact path and will not render anything else
     <div className="App">
-    <HomePage/>
+    <Switch>
+    
+      <Route exact={true} path='/' component ={HomePage}/>
+      <Route exact={true} path='/hats' component ={HatsPage}/>
+      <Route exact={true} path='/topics/:topicId' component ={TopicDetail}/>
+    </Switch>
+    
     </div>
     
   );
