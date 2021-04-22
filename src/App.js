f96d1a9f9fadccf581e0ac0afb07d6324423eb16
prasunroyy <44784505+prasunroyy@users.noreply.github.com>
2021-04-22 16:56:29 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from'./pages/homepage/homepage.component';
 import ShopPage from'./pages/shop/shop.component';
-
+import Header from'./components/header/header.component'
 
 
 
@@ -12,11 +12,12 @@ function App() {
   return (
     //switch will only match the exact path and will not render anything else
     <div className="App">
+    <Header/>
     <Switch>
     
       <Route exact={true} path='/' component ={HomePage}/>
       <Route exact={true} path='/shop' component ={ShopPage}/>
-      </Switch>
+    </Switch>
       
       </div>
       
