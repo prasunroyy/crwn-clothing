277acf118ae15cecfc56ded7ee62b2c13807a948
prasunroyy <44784505+prasunroyy@users.noreply.github.com>
2021-04-30 16:32:41 +0530

    updated data from array to object

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,12 @@
 import { createSelector } from "reselect";
 /////url gives us ID so we return string instead
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
 const selectShop = (state) => state.shop;
 
@@ -14,9 +14,13 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+  //hats jackets sneakers mens womens , same as the array view now
+);
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
