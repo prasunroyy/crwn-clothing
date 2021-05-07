bd20e8accdcfa9d92e8c9a4731eef8e0c1fa4df0
prasunroyy <44784505+prasunroyy@users.noreply.github.com>
2021-05-07 18:57:38 +0530

    separating our app into client folder and express to handle backend

---
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,46 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IluJMSJhOPR4NXZGEUVRstfXvMOxq6T0EEYsXiDwGOt2D1agD95dSiYx0ZIXNB5ilOFqRFwyBcjuEXUKnQqPcMI00Bm3D4UL1";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      currency="INR"
+      image="http://svgshare.com/i/CUz.svg"
+      description={`Your total is ₹${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+export default StripeCheckoutButton;
